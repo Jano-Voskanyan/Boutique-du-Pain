5ef60e5df21e31fd081c9b631405ba28523b8c52
Janibek <jano.vr@mail.ru>
2023-04-08 17:04:04 +0400

    delete main

---
+++ Boutique du Pain/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Boutique du Pain
+//
+//  Created by Janibek Voskanyan on 08.04.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
